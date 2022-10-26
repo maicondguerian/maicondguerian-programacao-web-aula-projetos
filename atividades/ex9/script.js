394e07359afd5ce1a9446357e5092d161fb1f1b4
maicondguerian <maicondguerian@gmail.com>
2022-10-26 16:14:38 -0300

    ex9

--- atividades/ex9/script.js
+++ atividades/ex9/script.js
@@ -1,43 +1,11 @@
 function tabuada(){
-    var nbr = Number(document.getElementById("number").value)
-        var vezes_1 = nbr * 1
-        vezes_1.innerHTML = (vezes_1)
-        console.log(vezes_1)
-
-        var vezes_2 = nbr * 2
-        vezes_2.innerHTML = vezes_2
-        console.log(vezes_2)
-
-        var vezes_3 = nbr * 3
-        
-        vezes_3.innerHTML = vezes_3
-        console.log(vezes_3)
-
-        var vezes_4 = nbr * 4
-        vezes_4.innerHTML = vezes_4
-        console.log(vezes_4)
-
-        var vezes_5 = nbr * 5
-        vezes_5.innerHTML = (vezes_5)
-        console.log(vezes_5)
-
-        var vezes_6 = nbr * 6
-        vezes_6.innerHTML = vezes_6
-        console.log(vezes_6)
-
-        var vezes_7 = nbr * 7
-        vezes_7.innerHTML = vezes_7
-        console.log(vezes_7)
-
-        var vezes_8 = nbr * 8
-        vezes_8.innerHTML = vezes_8
-        console.log(vezes_8)
-
-        var vezes_9 = nbr * 9
-        vezes_9.innerHTML = vezes_9
-        console.log(vezes_9)
-
-        var vezes_10 = nbr * 10
-        vezes_10.innerHTML = vezes_10
-        console.log(vezes_10)
-    }
+    var num = parseInt(document.getElementById("num").value);
+    var resposta = document.getElementById('resposta');
+    var tabuada='';
+  
+    for(var count=1; count<=10 ; count++)
+     tabuada += num+" x "+count+" = "+
+                 num*count+"<br />";
+    
+    resposta.innerHTML = tabuada;
+}
